afbad3ecca2f4027d9024ed1f611e65c07d91fe1
ROLLERBLVDE <80286986+harrell77@users.noreply.github.com>
2021-07-07 21:01:44 -0400

    WIN11CHECK Release
    WIN11CHECK helps check your PC for windows 11, just start it up accept to run as Administrator and wait! 
    
    If you believe that this is a virus look through the code if you like, I promise this isn't a virus in any sort of way at all!

---
+++ WIN11CHECK/README.md
@@ -0,0 +1,32 @@
+# Windows-11-Compatibility-Checker
+Checks if your system meets the requirements of Windows 11.
+
+This app features a better UI than WhyNotWin11.
+
+If you don't trust me you can decompile the app, it's not obfuscated. Also, f you Microsoft for flagging this as malware but refusing to add the PanOS malware to your db. This piece of software is safe to use on your host, ignore any false positives
+
+**Requirements**
+- .NET Framework 4.5 or later
+- Admin because the TPM status check command requires admin
+- Windows 7 or later
+
+**Known issues**
+- App will seem blurry on displays with a high DPI scaling. ~~I may fix this issue if I'm in a mood to release a separate build for such devices ;)~~ (Update: Version 2.0 Preview fixes this. 2.0 is currently in beta but will reach stable soon enough.)
+- App may hang or stop responding when checking DirectX version for the first time. Just be patient.
+
+![image](https://user-images.githubusercontent.com/63195743/123732321-50798000-d8cc-11eb-95d5-ed092e53e596.png)
+
+![image](https://user-images.githubusercontent.com/63195743/123904190-a7518900-d9a2-11eb-884c-fe067c99a086.png)
+
+![image](https://user-images.githubusercontent.com/63195743/123904208-b0425a80-d9a2-11eb-8f4c-9a6a08eb1244.png)
+
+
+**To-do list**
+
+Nothing rn, if you have something to suggest you can create an issue
+
+**Compiling from source code**
+
+Just open the solution in Visual Studio or JetBrains and click the (re)build button
+
+If you run this on Windows 11, you'd get a friendly message
---
+++ WIN11CHECK/src/Window1.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace Windows_11_Compatibility_Checker_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+            windows11.Source = new BitmapImage(new Uri(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\Orange Group\Windows 11 Compatibility Checker\Windows 11.png"));
+            RegistryKey checkwindowsbuild = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion");
+            if(Convert.ToInt32(checkwindowsbuild.GetValue("CurrentBuildNumber")) < 21390)
+            {
+                title.FontFamily = new FontFamily("Segoe UI");
+                title.FontWeight = FontWeights.Bold;
+                version.FontFamily = new FontFamily("Segoe UI");
+              
+                closeButton.FontFamily = new FontFamily("Segoe UI");
+            }
+            RegistryKey darklightmode = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Orange Group\Windows 11 Compatibility Checker");
+            if ((int)darklightmode.GetValue("DarkMode") == 1)
+            {
+                Background = Brushes.Black;
+                title.Foreground = Brushes.White;
+                version.Foreground = Brushes.White;
+               
+            }
+            
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
